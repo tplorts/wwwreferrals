fc04572ef3b2671d628d5ad80664c25981403283
Theodore Lorts <tplorts@gmail.com>
2015-01-14 19:42:37 -0500

    test DRF with default example--user API

--- wwwreferrals/urls.py
+++ wwwreferrals/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from referral_manager import urls as referral_urls
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wwwreferrals.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(referral_urls)),
 )
