66409a8725d825a9be4cd93d76ff80b3198b5600
Theodore Lorts <tplorts@gmail.com>
2015-01-15 08:30:44 -0500

    added core function of adding a new referral link

--- referral_manager/static/my/js/ng/controllers.js
+++ referral_manager/static/my/js/ng/controllers.js
@@ -4,10 +4,32 @@
     var cmod = angular.module('wwwReferralsApp.controllers', []);
 
     cmod.controller('LinkListCtlr', ['$scope', 'Restangular', function($scope, Restangular) {
+        var ReferralLinks = Restangular.all('links');
+        
         $scope.links = [];
-        Restangular.all('links').getList().then(function(fetchedLinks){
-            $scope.links = fetchedLinks;
-        });
+        $scope.newLink = {
+            title: ''
+        };
+        $scope.isPosting = false;
+        
+        function fetchLinks() {
+            ReferralLinks.getList().then(function(fetchedLinks){
+                $scope.links = fetchedLinks;
+            });
+        }
+        fetchLinks();
+
+        $scope.addLink = function(){
+            $scope.isPosting = true;
+            ReferralLinks.post($scope.newLink).then(function(){
+                fetchLinks();
+                $scope.newLink.title = '';
+                $scope.isPosting = false;
+            }, function(errorResponse){
+                $scope.errorMessage = errorResponse.data.title;
+                $scope.isPosting = false;
+            });
+        };
     }]);
 
 }());
