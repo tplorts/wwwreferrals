67e5a3107becb5c85321bce002158bfbe1b92329
Theodore Lorts <tplorts@gmail.com>
2015-01-14 20:43:40 -0500

    replaced the example API endpoint with one for referral links

--- referral_manager/urls.py
+++ referral_manager/urls.py
@@ -1,24 +1,23 @@
 from django.conf.urls import url, include
-from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from referral_manager.models import ReferralLink
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+class LinkSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
+        model = ReferralLink
+        fields = ('id', 'title', 'hits')
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
-# Routers provide an easy way of automatically determining the URL conf.
+class LinkViewSet(viewsets.ModelViewSet):
+    queryset = ReferralLink.objects.all()
+    serializer_class = LinkSerializer
+
+
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'links', LinkViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
