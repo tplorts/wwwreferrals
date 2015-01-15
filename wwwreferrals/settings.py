86d72fc9be64af6241b2416b9393df4f203d73db
Theodore Lorts <tplorts@gmail.com>
2015-01-14 19:22:48 -0500

    installed DRF

--- wwwreferrals/settings.py
+++ wwwreferrals/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -122,3 +123,13 @@ TEMPLATE_DEBUG = DEBUG
 
 # API key used for Google Maps etc.
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
