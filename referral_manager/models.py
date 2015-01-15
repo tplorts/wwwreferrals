f7d8c46fa81be7373f013fd0e23389dcda28ad0e
Theodore Lorts <tplorts@gmail.com>
2015-01-14 20:37:24 -0500

    created a basic referral link model

--- referral_manager/models.py
+++ referral_manager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ReferralLink(models.Model):
+    """
+    Specifies a route through which customer referrals can be tracked.
+    """
+
+    # The distinct part of the route, which identifies the referral link.
+    # Max of 126 to fit in PostgreSQL's short string requirement.
+    title = models.CharField(max_length=126)
+
+    # Counts the number of times this referral link has been used.
+    hits = models.PositiveIntegerField(default=0)
